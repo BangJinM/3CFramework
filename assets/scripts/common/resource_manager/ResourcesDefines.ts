b0884016af9880d04a7462c44454f99f69730f12
mabangjin <mabangjin@outlook.com>
2023-11-17 09:52:58 +0800

    * 注释

--- assets/scripts/common/resource_manager/ResourcesDefines.ts
+++ assets/scripts/common/resource_manager/ResourcesDefines.ts
@@ -4,14 +4,15 @@ export type AssetType = cc.Constructor<cc.Asset>
 export type LoadAssetCompleteFunc = (error: Error | null, asset: AssetCache) => void;
 export type LoadBundleCompleteFunc = (error: Error | null, asset: BundleCache) => void;
 
+/** 资源下载任务 */
 export class AssetLoadTask {
     completeFuncs: LoadAssetCompleteFunc[] = []
 }
-
+/** bundle 下载任务 */
 export class BundleLoadTask {
     loadBundleCompleteFunc: LoadBundleCompleteFunc[] = []
 }
-
+/** 缓存引用 */
 export class CacheRef {
     refCount = 0
 
@@ -27,14 +28,19 @@ export class CacheRef {
 }
 
 export class AssetCache extends CacheRef {
+    /** 路径 */
     url: string
+    /** 资源类型 */
     type: AssetType
+    /** 资源 */
     data: cc.Asset
-    loadAssetCompleteFunc: LoadAssetCompleteFunc
+    /** bundle缓存 */
     bundle: BundleCache
 }
 
 export class BundleCache extends CacheRef {
+    /** 路径或者名字 */
     url: string
+    /** bundle */
     bundle: cc.AssetManager.Bundle
 }
